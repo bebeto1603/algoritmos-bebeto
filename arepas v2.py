4d65c114b78681dc3acfad0ed5464f1590b508c6
bebeto1603 <jaimes.miguel@correo.unimet.edu.ve>
2024-09-17 07:33:26 -0400

    Add files via upload

---
+++ arepas v2.py
@@ -0,0 +1,21 @@
+agua = int(input("imdique el valor del agua"))
+harina= int(input ("indique el valor de la harina"))	      
+sal = agua/16/3
+aceite = agua/16
+bol = agua + sal + harina
+budare = bol + aceite
+
+print ("el agua es" ,agua)
+print ("la harina es" ,harina)
+sal = agua/16/3
+print("la sal es" ,sal)
+aceite = agua/16
+print ("el aceite es" ,aceite)
+bol = agua + sal + harina
+budare = bol + aceite
+print("la arepas finales son" ,budare)
+
+input ("presione enter para finalizar")
+
+
+
